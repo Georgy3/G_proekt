f87d97c9c660524b04eb19584b01b647934feca2
Georgy3 <egkt2@yandex.ru>
2018-12-14 19:07:11 +0300

    Добавил шаблон страницы бронирования

---
+++ zapros.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+
+
+
+</style>
+<title>Календарь</title>
+</head>
+<body>
+<?php
+
+<input type="date" class="form-control" id="date" name="date" placeholder="Дата" required>;
+?>
+</body>
+</html>
